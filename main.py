07d84efe64e62e3b6f44feec53e0cc2d5f22b051
Puupuls <paulspurvins@gmail.com>
2021-05-18 15:46:34 +0300

    Added upload

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
-from flask import Flask, Response, render_template
+import os
+
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './files/'
+os.makedirs('./files/', exist_ok=True)
 
 
 @app.route('/')
 def main():
     return render_template('index.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'videoFile' not in request.files:
+            print('No file part')
+        else:
+            file = request.files['videoFile']
+            # if user does not select file, browser also
+            # submit an empty part without filename
+            if file.filename == '':
+                print('No selected file')
+            elif file:
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    return redirect('/')
 
 
 app.run(debug=True)
